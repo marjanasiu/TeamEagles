50f047409dc63b372032f3c0afe0042d414e7e47
AjBhuiya <firoz11218@gmail.com>
2020-01-28 03:27:03 -0500

    project

---
+++ Generic/src/main/java/Reporting/ApplicationLog.java
@@ -0,0 +1,9 @@
+package Reporting;
+
+public class ApplicationLog {
+    public static void epicLogger(){
+        //TestLogger.log(epic);
+        TestLogger.log("Browser is launching");
+    }
+}
+
