78cde694359185dafb91763a3b92f8e45ac0004e
AjBhuiya <firoz11218@gmail.com>
2020-01-31 16:34:30 -0500

    eagles

--- AmazonWebStore/src/main/java/AmazonWeb/AmazonWebstore.java
+++ AmazonWebStore/src/main/java/AmazonWeb/AmazonWebstore.java
@@ -2,6 +2,10 @@ package AmazonWeb;
 
 import common.CommonAPI;
 import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AmazonWebstore extends CommonAPI {
 
@@ -13,12 +17,35 @@ public class AmazonWebstore extends CommonAPI {
     public void signIn() {
         this.driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span[1]")).click();
         this.driver.findElement(By.xpath("//*[@id=\"nav-flyout-ya-signin\"]/a/span")).click();
-
     }
 
-    public void searchTest() {
-        typeOnElement("twotabsearchtextbox", "matcha green tea");
-        clickOnElement("//*[@id=\"nav-search\"]/form/div[2]/div/input");
+    public void goToPrimePage() {
+        this.driver.findElement(By.xpath("//*[@id=\"nav-link-prime\"]")).click();
+    }
+
+    public void goToCart() {
+        this.driver.findElement(By.xpath("//*[@id=\"nav-cart\"]/span[3]")).click();
+    }
+
+    public void goToMenu() {
+        this.driver.findElement(By.xpath("//*[@id=\"nav-hamburger-menu\"]/i")).click();
+    }
+
+    public void changeLanguages() {
+        this.driver.findElement(By.xpath("//*[@id=\"icp-nav-flyout\"]/span/span[1]/span[2]")).click();
+        this.driver.findElement(By.xpath("//*[@id=\"nav-flyout-icp\"]/div[2]/a[1]/span/i")).click();
+    }
+
+    public void searchTwo() {
+        this.driver.findElement(By.id("twotabsearchtextbox")).sendKeys();
+    }
+
+    public void searchForBooks() {
+        this.driver.findElement(By.id("twotabsearchtextbox")).sendKeys();
+    }
+
+    public void goToTodaysDeal() {
+        this.driver.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[1]")).click();
     }
 
 
--- AmazonWebStore/src/test/java/AmazonWeb/AmazonWebTest.java
+++ AmazonWebStore/src/test/java/AmazonWeb/AmazonWebTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 
 public class AmazonWebTest extends CommonAPI {
 
-
     @Test
     public void amazonSearchEngineTest() throws InterruptedException {
         System.out.println(driver.getTitle());
@@ -13,7 +12,8 @@ public class AmazonWebTest extends CommonAPI {
         clickOnElement("//*[@id=\"nav-search\"]/form/div[2]/div/input");
         Thread.sleep(2000);
     }
- @Test
+
+    @Test
     public void search() {
         driver.get("https://amazon.com");
         AmazonWebstore hp = new AmazonWebstore();
@@ -25,9 +25,59 @@ public class AmazonWebTest extends CommonAPI {
         driver.get("https://amazon.com");
         AmazonWebstore hp = new AmazonWebstore();
         hp.signIn();
-
     }
 
+    @Test
+    public void goToTryPrimePage() {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.goToPrimePage();
+    }
 
+    @Test
+    public void goToCartPage() {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.goToCart();
+    }
 
+    @Test
+    public void goToMenuPage() {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.goToMenu();
+    }
+
+    @Test
+    public void changeLanguage() {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.changeLanguages();
+    }
+
+    /*@Test
+    public void goToCustomerService() {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.goToAmazonCustomerService();*/
+    @Test
+    public void searchForJava(String java) {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.searchTwo();
+    }
+
+    @Test
+    public void searchForBooks(String java) {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.searchForBooks();
+    }
+
+    @Test
+    public void clickTodaysDeal() {
+        driver.get("https://amazon.com");
+        AmazonWebstore hp = new AmazonWebstore();
+        hp.goToTodaysDeal();
+    }
 }
